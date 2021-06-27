139177115b387b78626faf9ac4e6ac0cef6eff86
Mashu Ajmera <mashuajmera@gmail.com>
2021-06-27 23:31:22 +0530

    order of menu items

--- src/components/nav.component.js
+++ src/components/nav.component.js
@@ -23,30 +23,30 @@ export default function Nav(props) {
         }}
     >
         <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1" icon={<UserOutlined />}>
-                Certificates
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                Diagnostics
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-                Information
-            </Menu.Item>
-            <Menu.Item key="4" icon={<BarChartOutlined />}>
                 Overview
             </Menu.Item>
-            <Menu.Item key="5" icon={<CloudOutlined />}>
-                Licenses
-            </Menu.Item>
-            <Menu.Item key="6" icon={<AppstoreOutlined />}>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
                 Accounts
             </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+                Certificates
+            </Menu.Item>
+            <Menu.Item key="4" icon={<BarChartOutlined />}>
+                Configuration
+            </Menu.Item>
+            <Menu.Item key="5" icon={<CloudOutlined />}>
+                Diagnostics
+            </Menu.Item>
+            <Menu.Item key="6" icon={<AppstoreOutlined />}>
+                Information
+            </Menu.Item>
             <Menu.Item key="7" icon={<TeamOutlined />}>
-                Networking
+                Licenses
             </Menu.Item>
             <Menu.Item key="8" icon={<ShopOutlined />}>
-                Configuration
+                Networking
             </Menu.Item>
         </Menu>
     </Sider>);
