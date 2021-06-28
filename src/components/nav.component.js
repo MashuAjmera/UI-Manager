4d4acaefb54e844e8adde0b91b9dfd9feef22675
Mashu Ajmera <mashuajmera@gmail.com>
2021-06-28 13:54:55 +0530

    navigation menu fixed

--- src/components/nav.component.js
+++ src/components/nav.component.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import {Link } from "react-router-dom";
 import { Layout, Menu } from 'antd';
 import {
   AppstoreOutlined,
@@ -12,45 +12,47 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 
-export default function Nav(props) {
-    const { Sider,Footer } = Layout;
-    return(
-    <Sider
-        style={{
-            overflow: 'auto',
-            height: '100vh',
-            position: 'fixed',
-            left: 0,
-        }}
-    >
-        <div className="logo" />
-        {console.log(props.match.path)}
-        <Menu theme="dark" mode="inline" selectedKeys={["/"]}>
-            <Menu.Item key="/" icon={<UserOutlined />}>
-                <Link to="/">Overview</Link>
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                Accounts
-            </Menu.Item>
-            <Menu.Item key="/certificates" icon={<UploadOutlined />}>
-                <Link to="/certificates">Certificates</Link>
-            </Menu.Item>
-            <Menu.Item key="4" icon={<BarChartOutlined />}>
-                Configuration
-            </Menu.Item>
-            <Menu.Item key="5" icon={<CloudOutlined />}>
-                Diagnostics
-            </Menu.Item>
-            <Menu.Item key="6" icon={<AppstoreOutlined />}>
-                Information
-            </Menu.Item>
-            <Menu.Item key="7" icon={<TeamOutlined />}>
-                Licenses
-            </Menu.Item>
-            <Menu.Item key="8" icon={<ShopOutlined />}>
-                Networking
-            </Menu.Item>
-        </Menu>
-            <Footer style={{position: 'fixed',bottom:0, background: 'none', color: 'rgba(255,255,255,0.55)', paddingLeft:24}}>© 2021 ABB Ltd.</Footer>
-    </Sider>);
+export default class Nav extends Component {
+    render(){
+        const { Sider, Footer } = Layout;
+        return(
+        <Sider
+            style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+            }}
+        >
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={[window.location.pathname]}>
+                <Menu.Item key="/" icon={<UserOutlined />}>
+                    <span>Overview</span><Link to="/"/>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                    Accounts
+                </Menu.Item>
+                <Menu.Item key="/certificates" icon={<UploadOutlined />}>
+                    <span>Certificates</span><Link to="/certificates"/>
+                </Menu.Item>
+                <Menu.Item key="4" icon={<BarChartOutlined />}>
+                    Configuration
+                </Menu.Item>
+                <Menu.Item key="5" icon={<CloudOutlined />}>
+                    Diagnostics
+                </Menu.Item>
+                <Menu.Item key="6" icon={<AppstoreOutlined />}>
+                    Information
+                </Menu.Item>
+                <Menu.Item key="7" icon={<TeamOutlined />}>
+                    Licenses
+                </Menu.Item>
+                <Menu.Item key="8" icon={<ShopOutlined />}>
+                    Networking
+                </Menu.Item>
+            </Menu>
+                <Footer style={{position: 'fixed',bottom:0, background: 'none', color: 'rgba(255,255,255,0.55)', paddingLeft:24}}>© 2021 ABB Ltd.</Footer>
+        </Sider>
+        );
+    }
 }
