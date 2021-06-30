44b3b0a13f0750800ce2c92be641f38c4d1523b8
Mashu Ajmera <mashuajmera@gmail.com>
2021-06-30 14:51:46 +0530

    mentioning paths and locations in env file

--- server.py
+++ server.py
@@ -12,9 +12,6 @@ VERIFY_USER = True
 
 API_HOST = "0.0.0.0"
 API_PORT = 5000
-API_CRT = "server_cert.pem"
-API_KEY = "server_key.pem"
-API_CA_T = "server_cert.pem"
 
 app = Flask(__name__,static_folder='build')
 
@@ -62,21 +59,21 @@ def info():
 @app.route("/api/uploader" , methods=['POST'])
 def uploader():
     f = request.files['file']
-    f.save(os.path.join(os.environ.get("FLASK_UPLOAD_FOLDER"), secure_filename(f.filename)))
+    f.save(os.path.join(os.environ.get("API_UPLOAD_FOLDER"), secure_filename(f.filename)))
     return "Uploaded successfully!"
 
 @app.route("/api/downloader")
 def downloader():
-    return send_file('/home/mashu/ABB/alice.p12',as_attachment=True)
+    return send_file(os.path.join(os.environ.get("API_DOWNLOAD_FOLDER"), 'alice.p12'),as_attachment=True)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if os.environ.get("FLASK_ENV")!='development':
-        if path != "" and os.path.exists(app.static_folder + '/' + path):
-            return send_from_directory(app.static_folder, path)
-        else:
-            return send_from_directory(app.static_folder, 'index.html')
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def serve(path):
+#     if os.environ.get("FLASK_ENV")!='development':
+#         if path != "" and os.path.exists(app.static_folder + '/' + path):
+#             return send_from_directory(app.static_folder, path)
+#         else:
+#             return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     context = None
@@ -84,9 +81,9 @@ if __name__ == "__main__":
         context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         if VERIFY_USER:
             context.verify_mode = ssl.CERT_REQUIRED
-            context.load_verify_locations(API_CA_T)
+            context.load_verify_locations(os.environ.get("API_CA_T"))
         try:
-            context.load_cert_chain(API_CRT, API_KEY)
+            context.load_cert_chain(os.environ.get("API_CRT"), os.environ.get("API_KEY"))
         except Exception as e:
             sys.exit("Error starting flask server. " + "Missing cert or key. Details: {}".format(e))
     serving.run_simple(API_HOST, API_PORT, app, ssl_context=context)
