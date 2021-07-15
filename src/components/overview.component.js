b0b6d14541e1600553d972c3013b5a7379c1ab1a
Mashu Ajmera <mashuajmera@gmail.com>
2021-07-15 22:28:01 +0530

    input field to take IP MAC

--- src/components/overview.component.js
+++ src/components/overview.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Layout, Button, Table, Typography, message, Skeleton } from 'antd';
+import { Layout, Table, Typography, message, Skeleton, Input } from 'antd';
 
 export default class Overview extends Component {
   state = { info: null, loading: true };
@@ -27,24 +27,24 @@ export default class Overview extends Component {
       .catch(error=>console.log(error))
   }
 
-  setmac = () => {
-    // mac='08:00:27:ec:c0:c9'
+  setmac = (value) => {
+    // let value ='08:00:27:ec:c0:c9'
     fetch('/api/network/mac', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ mac: '08:01:27:ec:c0:c9' })
+      body: JSON.stringify({ mac: value })
     }).then(response => response.json())
       .then(data => message.success(data))
       .then(() => this.getmac())
       .catch(err => message.warning(err));
   };
 
-  setip = () => {
-    // ip='192.168.29.201'
+  setip = (value) => {
+    // let value='192.168.29.201'
     fetch('/api/network/ip', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ ip: '192.168.29.200' })
+      body: JSON.stringify({ ip: value })
     }).then(response => response.json())
       .then(data => message.success(data))
       .then(() => this.getip())
@@ -74,8 +74,8 @@ export default class Overview extends Component {
       <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
         <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
           <Title>Welcome to the dashboard!</Title>
-          <Button type="primary" onClick={this.setmac}>Change MAC</Button>
-          <Button type="primary" style={{ marginLeft: 8 }} onClick={this.setip}>Change IP</Button>
+          <Input.Search placeholder="Enter MAC Address" onSearch={this.setmac} enterButton="Change" style={{ width: 250 }}  />
+          <Input.Search placeholder="Enter IP Address" onSearch={this.setip} enterButton="Add" style={{ marginLeft: 8, width: 250 }}  />
           <Skeleton loading={this.state.loading} active title={{ width: '100%' }} paragraph={{ rows: 10, width: '100%' }}>
             {this.state.info && <Table columns={columns} dataSource={Object.entries(this.state.info)} style={{ marginTop: 30 }} />}
           </Skeleton>
