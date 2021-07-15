15a0d492c88205fa597aa6dcb1f6167dd7d15043
Mashu Ajmera <mashuajmera@gmail.com>
2021-07-15 22:46:48 +0530

    changed into functional component

--- src/components/database.component.js
+++ src/components/database.component.js
@@ -1,46 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { Layout, Row, Col } from 'antd';
 import Buckets from "./buckets.component.js"
 import OrganizationMembers from "./members.component.js"
 import Users from "./users.component.js"
 import Organizations from "./organizations.component.js"
 
-export default class Database extends Component {
-  state = { org: {}, user: {} };
+export default function Database() {
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+    }
+  };
 
-  componentDidMount() {
-
-    // fetch('/api/db/organization')
-    //   // .then(response => response.json())
-    //   .then((data) => this.setState({orgs:data}))
-    //   .catch(error => message.warning({ content: error}));
-    // fetch('/api/db/user')
-    //   // .then(response => response.json())
-    //   .then((data) => this.setState({users:data}))
-    //   .catch(error => message.warning({ content: error}));
-  }
-
-  render() {
-
-    const rowSelection = {
-      onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-      }
-    };
-
-    return (
-      <Layout className="site-layout">
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col span={12} className="gutter-row">
-            <Organizations rowSelection={rowSelection} />
-          </Col>
-          <Col span={12} className="gutter-row">
-            {/* <Users rowSelection={rowSelection} /> */}
-          </Col>
-        </Row>
-        {/* <Buckets rowSelection={rowSelection} /> */}
-        {/* <OrganizationMembers rowSelection={rowSelection} /> */}
-      </Layout>
-    );
-  }
+  return (
+    <Layout className="site-layout">
+      <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+        <Col span={12} className="gutter-row">
+          <Organizations rowSelection={rowSelection} />
+        </Col>
+        <Col span={12} className="gutter-row">
+          {/* <Users rowSelection={rowSelection} /> */}
+        </Col>
+      </Row>
+      <Buckets rowSelection={rowSelection} />
+      {/* <OrganizationMembers rowSelection={rowSelection} /> */}
+    </Layout>
+  );
 }
